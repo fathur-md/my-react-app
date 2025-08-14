9b4700c113bbed2384a557e05c82e34bcbc57d1c
Fathur Muhammad <fathurmh@98gmail.com>
2025-08-14 16:59:44 +0700

    Update div class to min-h-dvh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center antialiased p-4 h-screen">
+      <div className="flex items-center justify-center antialiased p-4 min-h-dvh">
         <h1 className="text-4xl">Hello from App reactjs!</h1>
       </div>
     </>
