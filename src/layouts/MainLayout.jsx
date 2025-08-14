e1c45d751584a8f02ad32b9f4e90a74856285e31
Fathur Muhammad <fathurmh@98gmail.com>
2025-08-14 19:43:24 +0700

    add animation on pages change or refresh

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const MainLayout = () => {
+  const location = useLocation();
+
   return (
     <div className="min-h-dvh flex flex-col antialiased">
       <Navbar />
-      <main className="flex-1 flex">
-        <Outlet />
-      </main>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -30 }}
+          transition={{ duration: 0.45, ease: "easeOut" }}
+          className="flex-1 flex"
+        >
+          <Outlet />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
