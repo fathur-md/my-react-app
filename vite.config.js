4957f602669d97c65d38e97357ff02ec24e7f017
Fathur Muhammad <fathurmh@98gmail.com>
2025-08-14 18:03:04 +0700

    fixing routes 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-react-app",
   plugins: [react(), tailwindcss()],
+  base: "/my-react-app",
 });
