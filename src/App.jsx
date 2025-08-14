534309cc4cba8d936615e847d0a8974776f7d08d
Fathur Muhammad <fathurmh@98gmail.com>
2025-08-14 16:38:02 +0700

    Initial commit: React + Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center antialiased p-4 h-screen">
+        <h1 className="text-4xl">Hello from App reactjs!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
