aad87d207f338959fa7e225945c024268b4ce9d3
Fathur Muhammad <fathurmh@98gmail.com>
2025-08-14 19:04:18 +0700

    change routes from browser router to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
-import { Navbar } from "./components/Navbar";
 import { MainLayout } from "./layouts/MainLayout";
 
 function App() {
   return (
-    <BrowserRouter basename="/my-react-app">
+    <HashRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
