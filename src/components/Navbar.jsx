9e3b927cbfa4fd2bdd10eaf48fa81b3cf0569973
Fathur Muhammad <fathurmh@98gmail.com>
2025-08-14 19:59:08 +0700

    add background gradient animation on navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 inset-x-0 z-50 h-12 flex">
+    <nav className="fixed top-0 inset-x-0 z50 h-12 flex">
       <div className="w-full bg-white/60 dark:bg-zinc-950/60 backdrop-blur-3xl shadow-sm flex px-4">
         <div className="flex items-center justify-between w-full container mx-auto">
           <Link to="/" className="flex items-center gap-2">
             <Frame />
-            <h1 className="font-light text-lg">
+            <h1 className="font-light text-xl gradient-animated gradient-x">
               Fathur. <span className="font-semibold text-base">DEV</span>
             </h1>
           </Link>
